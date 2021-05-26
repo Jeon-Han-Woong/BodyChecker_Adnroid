8d98faa78dafeebdff371f2394240a93e5387d59
Jeon-Han-Woong <gksnd506@naver.com>
2021-05-26 18:02:13 +0900

    운동 페이지 하단 버튼 수정 - Woong

--- app/src/main/java/org/ict/bodychecker/ExerciseActivity.java
+++ app/src/main/java/org/ict/bodychecker/ExerciseActivity.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -26,6 +27,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.button.MaterialButton;
+
 import org.ict.bodychecker.ValueObject.ExerciseVO;
 import org.ict.bodychecker.retrofit.RetrofitClient;
 import org.ict.bodychecker.retrofit.RetrofitInterface;
@@ -33,6 +36,8 @@ import org.ict.bodychecker.retrofit.RetrofitInterface;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -41,6 +46,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class ExerciseActivity extends AppCompatActivity {
 
     private View dialog;
@@ -55,17 +61,27 @@ public class ExerciseActivity extends AppCompatActivity {
     String [] exeritems = {"걷기", "달리기", "계단 오르기", "스쿼트", "윗몸 일으키기", "훌라후프"};
 
     float [] exerkcal = {0.067f, 0.123f, 0.123f, 0.123f, 0.14f, 0.07f};
-    String selectexer, today;
+    String selectexer;
     float selectitem;
 
     Button accountkcal;
+
+    LocalDateTime today = LocalDateTime.now();
+    DateTimeFormatter dbFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    DateTimeFormatter btnFormat = DateTimeFormatter.ofPattern("MM/dd");
+
+    final String TIME = "T00:00:00";
+    String date = dbFormat.format(today);
 
     int temp_min, sel_index, temp_Eno;
     float temp_kg = 87.4f;
     int temp_result, temp_result_kcal = 0;
 
+    View exer_calendar;
+    DatePicker exerDatePick;
     EditText edtminute;
     TextView resultkcal, consumeKcal;
+    MaterialButton eMoreDaysAgoBtn, eSixDaysAgoBtn, eFiveDaysAgoBtn, eFourDaysAgoBtn, eThreeDaysAgoBtn, eTwoDaysAgoBtn, eOneDaysAgoBtn, eTodayBtn;
     LinearLayout newExerciseBtn;
 
     List<Integer> sel_spinner;
@@ -77,11 +93,10 @@ public class ExerciseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
-        today = String.valueOf(LocalDate.now());
 //        today = sdf.format(new Date());
         sel_spinner = new ArrayList<>();
 
-        Toast.makeText(this, today, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, date, Toast.LENGTH_SHORT).show();
 
         init();
 //        getData();
@@ -93,11 +108,31 @@ public class ExerciseActivity extends AppCompatActivity {
 
         newExerciseBtn = (LinearLayout) findViewById(R.id.newExerciseBtn);
 
+        exer_calendar = (View) View.inflate(ExerciseActivity.this, R.layout.exercise_calendar, null);
+
+        eMoreDaysAgoBtn = (MaterialButton) findViewById(R.id.eMoreDaysAgoBtn);
+        eSixDaysAgoBtn = (MaterialButton) findViewById(R.id.eSixDaysAgoBtn);
+        eFiveDaysAgoBtn = (MaterialButton) findViewById(R.id.eFiveDaysAgoBtn);
+        eFourDaysAgoBtn = (MaterialButton) findViewById(R.id.eFourDaysAgoBtn);
+        eThreeDaysAgoBtn = (MaterialButton) findViewById(R.id.eThreeDaysAgoBtn);
+        eTwoDaysAgoBtn = (MaterialButton) findViewById(R.id.eTwoDaysAgoBtn);
+        eOneDaysAgoBtn = (MaterialButton) findViewById(R.id.eOneDaysAgoBtn);
+        eTodayBtn = (MaterialButton) findViewById(R.id.eTodayBtn);
+
+        eTodayBtn.setText(setBtnDate(0));
+        eOneDaysAgoBtn.setText(setBtnDate(-1));
+        eTwoDaysAgoBtn.setText(setBtnDate(-2));
+        eThreeDaysAgoBtn.setText(setBtnDate(-3));
+        eFourDaysAgoBtn.setText(setBtnDate(-4));
+        eFiveDaysAgoBtn.setText(setBtnDate(-5));
+        eSixDaysAgoBtn.setText(setBtnDate(-6));
+
+
         retrofitClient = RetrofitClient.getInstance();
 
         retrofitInterface = RetrofitClient.getRetrofitInterface();
 
-        retrofitInterface.getDailyExer(today).enqueue(new Callback<List<ExerciseVO>>() {
+        retrofitInterface.getDailyExer(date).enqueue(new Callback<List<ExerciseVO>>() {
             @Override
             public void onResponse(Call<List<ExerciseVO>> call, Response<List<ExerciseVO>> response) {
                 if (response.isSuccessful()) {
@@ -132,6 +167,88 @@ public class ExerciseActivity extends AppCompatActivity {
             }
         });
 
+        eTodayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                today = LocalDateTime.now();
+                date = dbFormat.format(today);
+                getDBList(date);
+            }
+        });//today.onclick
+
+        eOneDaysAgoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                date = dbFormat.format(today.plusDays(-1));
+                getDBList(date);
+            }
+        });//oneDayAgo.onclick
+
+        eTwoDaysAgoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                date = dbFormat.format(today.plusDays(-2));
+                getDBList(date);
+            }
+        });//oneDayAgo.onclick
+
+        eThreeDaysAgoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                date = dbFormat.format(today.plusDays(-3));
+                getDBList(date);
+            }
+        });//oneDayAgo.onclick
+
+        eFourDaysAgoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                date = dbFormat.format(today.plusDays(-4));
+                getDBList(date);
+            }
+        });//oneDayAgo.onclick
+
+        eFiveDaysAgoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                date = dbFormat.format(today.plusDays(-5));
+                getDBList(date);
+            }
+        });//oneDayAgo.onclick
+
+        eSixDaysAgoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                date = dbFormat.format(today.plusDays(-6));
+                getDBList(date);
+            }
+        });//oneDayAgo.onclick
+
+        eMoreDaysAgoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(ExerciseActivity.this);
+                dlg.setTitle("날짜 선택");
+                if(exer_calendar.getParent() != null) {
+                    ((ViewGroup) exer_calendar.getParent()).removeView(exer_calendar);
+                }
+                dlg.setView(exer_calendar);
+
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        exerDatePick = (DatePicker) exer_calendar.findViewById(R.id.exerDatePick);
+
+                        date = exerDatePick.getYear()+"-"+String.format("%02d", exerDatePick.getMonth()+1)+"-"+String.format("%02d", exerDatePick.getDayOfMonth())+TIME;
+                        getDBList(date);
+                    }
+                });//dlg.positive
+
+                dlg.setNegativeButton("취소", null);
+
+                dlg.show();
+            }
+        });
 
 
 
@@ -144,109 +261,119 @@ public class ExerciseActivity extends AppCompatActivity {
 //        adapter = new RecyclerAdapter();
 //        recyclerView3.setAdapter(adapter);
 
-                newExerciseBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+        newExerciseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 //                        Toast.makeText(getActivity().getApplicationContext(), "zz", Toast.LENGTH_SHORT).show();
-                        dialog = (View) View.inflate(ExerciseActivity.this, R.layout.exercise_dialog, null);
+                dialog = (View) View.inflate(ExerciseActivity.this, R.layout.exercise_dialog, null);
 
-                        AlertDialog.Builder dlg = new AlertDialog.Builder(ExerciseActivity.this);
-                        spinner_exer = (Spinner) dialog.findViewById(R.id.exer_spinner);
-                        edtminute = (EditText) dialog.findViewById(R.id.edtminute);
-                        resultkcal = (TextView) dialog.findViewById(R.id.resultkcal);
-                        accountkcal = (Button) dialog.findViewById(R.id.accountkcal);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(ExerciseActivity.this);
+                spinner_exer = (Spinner) dialog.findViewById(R.id.exer_spinner);
+                edtminute = (EditText) dialog.findViewById(R.id.edtminute);
+                resultkcal = (TextView) dialog.findViewById(R.id.resultkcal);
+                accountkcal = (Button) dialog.findViewById(R.id.accountkcal);
 
-                        retrofitInterface.getNewEno().enqueue(new Callback<Integer>() {
-                            @Override
-                            public void onResponse(Call<Integer> call, Response<Integer> response) {
-                                temp_Eno = response.body() + 1;
-                                Toast.makeText(ExerciseActivity.this, temp_Eno+"", Toast.LENGTH_SHORT).show();
-                                Log.d("ENO = ", temp_Eno + "");
-                            }
+                retrofitInterface.getNewEno().enqueue(new Callback<Integer>() {
+                    @Override
+                    public void onResponse(Call<Integer> call, Response<Integer> response) {
+                        temp_Eno = response.body() + 1;
+                        Toast.makeText(ExerciseActivity.this, temp_Eno+"", Toast.LENGTH_SHORT).show();
+                        Log.d("ENO = ", temp_Eno + "");
+                    }
 
-                            @Override
-                            public void onFailure(Call<Integer> call, Throwable t) {
+                    @Override
+                    public void onFailure(Call<Integer> call, Throwable t) {
 
-                            }
-                        });
-                        
-
-
-                        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(ExerciseActivity.this, android.R.layout.simple_spinner_item, exeritems);
-
-                        ExerciseVO newExerData = new ExerciseVO();
-
-                        spinner_exer.setAdapter(adapter1);
-
-                        spinner_exer.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                            @Override
-                            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                                selectitem = exerkcal[i];
-                                selectexer = exeritems[i];
-                                sel_index = i;
-                            }
-
-                            @Override
-                            public void onNothingSelected(AdapterView<?> adapterView) {
-
-                            }
-                        });
-
-
-                        accountkcal.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                try {
-
-                                    temp_min = Integer.parseInt(edtminute.getText().toString());
-                                    temp_result = Math.round(temp_kg * (float) temp_min * selectitem);
-                                    resultkcal.setText(temp_result + " kcal");
-                                } catch (Exception e) {
-                                    Toast.makeText(getApplicationContext(), "시간을 입력해주세요.", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-
-
-                        dlg.setTitle("운동 기록 추가");
-
-                        dlg.setView(dialog);
-
-                        dlg.setPositiveButton("추가", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                newExerData.setEname(selectexer);
-                                newExerData.setEtime(Integer.parseInt(edtminute.getText().toString()));
-                                newExerData.setEkcal(temp_result);
-                                newExerData.setEdate(today);
-                                newExerData.setMno(1);
-                                newExerData.setEno(temp_Eno);
-
-                                temp_result_kcal += temp_result;
-
-                                consumeKcal.setText(temp_result_kcal + " kcal");
-                                sel_spinner.add(sel_index);
-
-                                retrofitInterface.registerExer(newExerData).enqueue(new Callback<ExerciseVO>() {
-                                    @Override
-                                    public void onResponse(Call<ExerciseVO> call, Response<ExerciseVO> response) {
-                                        Toast.makeText(ExerciseActivity.this, "새 운동 입력", Toast.LENGTH_SHORT).show();
-                                    }
-                                    @Override
-                                    public void onFailure(Call<ExerciseVO> call, Throwable t) {
-                                        Log.d("오류", t+"");
-                                    }
-                                });
-                                adapter.addItem(newExerData);
-                                adapter.notifyDataSetChanged();
-                            }
-                        });
-
-                        dlg.setNegativeButton("취소", null);
-
-                        dlg.show();
                     }
                 });
+
+
+
+                ArrayAdapter<String> adapter1 = new ArrayAdapter<>(ExerciseActivity.this, android.R.layout.simple_spinner_item, exeritems);
+
+                ExerciseVO newExerData = new ExerciseVO();
+
+                spinner_exer.setAdapter(adapter1);
+
+                spinner_exer.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        selectitem = exerkcal[i];
+                        selectexer = exeritems[i];
+                        sel_index = i;
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+
+
+                accountkcal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        try {
+
+                            temp_min = Integer.parseInt(edtminute.getText().toString());
+                            temp_result = Math.round(temp_kg * (float) temp_min * selectitem);
+                            resultkcal.setText(temp_result + " kcal");
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "시간을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+                dlg.setTitle("운동 기록 추가");
+
+                dlg.setView(dialog);
+
+                dlg.setPositiveButton("추가", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        newExerData.setEname(selectexer);
+                        newExerData.setEtime(Integer.parseInt(edtminute.getText().toString()));
+                        newExerData.setEkcal(temp_result);
+                        newExerData.setEdate(date);
+                        newExerData.setMno(1);
+                        newExerData.setEno(temp_Eno);
+
+                        temp_result_kcal += temp_result;
+
+                        consumeKcal.setText(temp_result_kcal + " kcal");
+                        sel_spinner.add(sel_index);
+
+                        retrofitInterface.registerExer(newExerData).enqueue(new Callback<ExerciseVO>() {
+                            @Override
+                            public void onResponse(Call<ExerciseVO> call, Response<ExerciseVO> response) {
+                                Toast.makeText(ExerciseActivity.this, "새 운동 입력", Toast.LENGTH_SHORT).show();
+                            }
+                            @Override
+                            public void onFailure(Call<ExerciseVO> call, Throwable t) {
+                                Log.d("오류", t+"");
+                            }
+                        });
+                        adapter.addItem(newExerData);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+                dlg.setNegativeButton("취소", null);
+
+                dlg.show();
+            }
+        });
+
+
+    }
+
+    private String setBtnDate(int n) {
+        return btnFormat.format(today.plusDays(n));
+    }//getDate
+
+    private void getDBList(String date) {
+
     }
 
 
